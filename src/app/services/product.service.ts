c50f9e693282a946e9539e1e9e925c6b7d9d68cf
mdmostafa <mdmostafaalmahmud@gmail.com>
2021-05-27 20:04:05 +0600

    order and cart service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,17 +2,30 @@ import { Injectable } from '@angular/core';
 // import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { IProductServer } from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   private SERVER_URL = environment.SERVER_URL;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // Load all products from server
-  getProducts(n:number = 10) {
+  getProducts() {
     return this.http.get(`${this.SERVER_URL}/products`);
+  }
+
+  /* GET SINGLE PRODUCT FROM BACKEND*/
+  getProduct(id: string): Observable<IProductServer> {
+    return this.http.get<IProductServer>(this.SERVER_URL + '/products/' + id);
+  }
+
+  /*GET PRODUCTS FROM ONE CATEGORY */
+  getProductsByCategory(catName: string): Observable<IProductServer[]> {
+    return this.http.get<IProductServer[]>(
+      this.SERVER_URL + '/products/category/' + catName
+    );
   }
 }
